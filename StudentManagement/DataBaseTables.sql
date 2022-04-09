df96626ccff33694948eb46daf44ba563137c6fc
ashwinkol <ashwinkolkar22@gmail.com>
2022-04-09 15:05:03 +0530

    New Db Added

--- StudentManagement/DataBaseTables.sql
+++ StudentManagement/DataBaseTables.sql
@@ -33,7 +33,7 @@ alter table Attendance add constraint Student_Id FOREIGN KEY ( Student_Id ) refe
 alter table Attendance add constraint Course_Id  FOREIGN KEY ( Course_Id  ) references Course(CourseId);
 
 
-create table Time_Table(Day varchar(20),Date DATE,ZoomId varchar(20),zoomPassword varchar(20),Course int(2));
+create table Time_Table(id int not null auto_increment ,Day varchar(20),Date DATE,ZoomId varchar(20),zoomPassword varchar(20),Course int(2),primary key(id));
 alter table Time_Table add constraint Course FOREIGN KEY ( Course ) references Course(CourseId);
 
 
